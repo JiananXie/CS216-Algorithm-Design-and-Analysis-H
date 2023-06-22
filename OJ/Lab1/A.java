b6cc2db4d0f116589766fa53cd36212a263df9e3
Jianan Xie <101252062+JiananXie@users.noreply.github.com>
2023-06-22 21:12:27 +0800

    Update A.java

--- OJ/Lab1/A.java
+++ OJ/Lab1/A.java
@@ -1,109 +1,89 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int capacity,t;
-    static node first=new node();
-    static node last=new node();
-    static HashMap<Integer,node> map=new HashMap<>(capacity);
     public static void main(String[] args) {
         QReader in = new QReader();
         QWriter out = new QWriter();
-        capacity = in.nextInt();
-        int m = in.nextInt();
-        t = in.nextInt();
-        first.next=last;
-        last.prev=first;
-        for(int i=0;i<m;i++){
-            int op=in.nextInt();
-            check(i);
-            if(op==1){
-                int k=in.nextInt();
-                int value=get(k,i);
-                out.println(value);
-            }else{
-                int k=in.nextInt();
-                int value=in.nextInt();
-                put(k,value,i);
+        int n=in.nextInt();
+        man[] men=new man[n+1];
+        woman[] women=new woman[n+1];
+        Map<String,Integer> map_m=new HashMap<>();
+        Map<String,Integer> map_w=new HashMap<>();
+        Queue<man> queue=new LinkedList<>();
+        int[] engage=new int[n+1];
+        for(int i=1;i<=n;i++){
+            String name=in.next();
+            men[i]=new man(i,name,n);
+            map_m.put(name,i);
+            queue.add(men[i]);
+        }
+        for(int i=1;i<=n;i++){
+            String name=in.next();
+            women[i]=new woman(i,name,n);
+            map_w.put(name,i);
+        }
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= n; j++) {
+                men[i].preference[j] = map_w.get(in.next());
             }
         }
-        if(first.next!=last){
-            node cur=last.prev;
-            while(cur!=first){
-                out.print(cur.value+" ");
-                cur=cur.prev;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                women[i].preference[j]= map_m.get(in.next());
+                women[i].preference_inverse[women[i].preference[j]]=j;
             }
+        }
+        while(!queue.isEmpty()){
+            man m=queue.poll();
+            m.count++;
+            if(m.count==n+1) break;
+            else{
+                woman w=women[m.preference[m.count]];
+                if(w.husband==0){
+                    engage[m.k]=w.k;
+                    w.husband=m.k;
+                }else{
+                    if(w.preference_inverse[m.k]<w.preference_inverse[w.husband]){
+                        engage[m.k]=w.k;
+                        queue.add(men[w.husband]);
+                        w.husband=m.k;
+                    }else{
+                        queue.add(m);
+                    }
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            out.print(men[i].name+" "+women[engage[i]].name);
+            out.println("");
         }
         out.close();
     }
-    public static int get(int key, int time){
-        node n=map.get(key);
-        if(n!=null){
-            n.time=time;
-            n.prev.next=n.next;
-            n.next.prev=n.prev;
-            n.next=first.next;
-            first.next.prev=n;
-            first.next=n;
-            n.prev=first;
-            return n.value;
-        }else{
-            return -1;
-        }
-    }
-
-    public static void put(int key,int value,int time){
-        node n=map.get(key);
-        if(n!=null){
-            n.time=time;
-            n.value=value;
-            n.prev.next=n.next;
-            n.next.prev=n.prev;
-            n.next=first.next;
-            first.next.prev=n;
-            first.next=n;
-            n.prev=first;
-        }else{
-            if(map.size()==capacity){
-                map.remove(last.prev.key);
-                last.prev.prev.next=last.prev.next;
-                last.prev.next.prev=last.prev.prev;
-            }
-            node n_node=new node(key,value);
-            n_node.time=time;
-            map.put(key,n_node);
-            n_node.next=first.next;
-            first.next.prev=n_node;
-            first.next=n_node;
-            n_node.prev=first;
-        }
-    }
-    public static void check(int time){
-        node cur=last.prev;
-        while(cur!=first){
-            if(time-cur.time>t){
-                map.remove(cur.key);
-                cur.prev.next=cur.next;
-                cur.next.prev=cur.prev;
-            }else{
-                break;
-            }
-            cur=cur.prev;
-        }
+}
+class man{
+    int k;
+    String name;
+    int[] preference;
+    int count=0;
+    int wife=0;
+    public man(int k, String name, int n) {
+        this.k = k;
+        this.name = name;
+        this.preference = new int[n+1];
     }
 }
-class node{
-    int key;
-    int value;
-    int time=0;
-    node prev;
-    node next;
-    public node() {
-    }
-    public node(int key, int value) {
-        this.key = key;
-        this.value = value;
+class woman{
+    int k;
+    String name;
+    int[] preference;
+    int[] preference_inverse;
+    int husband=0;
+    public woman(int k, String name, int n) {
+        this.k = k;
+        this.name = name;
+        this.preference = new int[n+1];
+        this.preference_inverse = new int[n+1];
     }
 }
 class QReader {
